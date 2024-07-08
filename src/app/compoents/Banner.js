e4310ef24fbc7967034db59e3f73edab274cdffd
muHammadHammadali786Ha <sunm25377@gmail.com>
2024-07-08 10:02:07 -0700

    first commit

---
+++ src/app/compoents/Banner.js
@@ -0,0 +1,12 @@
+// import Image from 'next/image'
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className='w-full'>
+        <img src={'/banner1.jpg'} className='w-full h-[18rem]'   alt='banner'/>
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/app/tshirt/page.js
@@ -0,0 +1,83 @@
+import React from 'react';
+
+const Tshirt = () => {
+  const list = [
+    {
+      id: 1,
+      img: "/tshirt1.jpg",
+      name: "The Catalyzer",
+      category: "tShirt"
+    },
+    {
+      id: 2,
+      img: "/tshirt1.jpg",
+      name: "The Catalyzer",
+      category: "tShirt"
+    },
+    {
+      id: 3,
+      img: "/tshirt1.jpg",
+      name: "The Catalyzer",
+      category: "tShirt"
+    },
+    {
+      id: 4,
+      img: "/tshirt1.jpg",
+      name: "The Catalyzer",
+      category: "tShirt"
+    },
+    {
+      id: 5,
+      img: "/tshirt1.jpg",
+      name: "The Catalyzer",
+      category: "tShirt"
+    },
+    {
+      id: 6,
+      img: "/tshirt1.jpg",
+      name: "The Catalyzer",
+      category: "tShirt"
+    },
+    {
+      id: 7,
+      img: "/tshirt1.jpg",
+      name: "The Catalyzer",
+      category: "tShirt"
+    },
+    {
+      id: 8,
+      img: "/tshirt1.jpg",
+      name: "The Catalyzer",
+      category: "tShirt"
+    }
+  ];
+
+  return (
+    <div>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            {
+              list.map((item, index) => {
+                return (
+                  <div className="lg:w-1/4 md:w-1/2 p-4 w-full" key={index}>
+                    <a className="block relative h-48 rounded overflow-hidden">
+                      <img alt="ecommerce" className="w-full h-full block" src={item.img} />
+                    </a>
+                    <div className="mt-4">
+                      <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">{item.category}</h3>
+                      <h2 className="text-gray-900 title-font text-lg font-medium">{item.name}</h2>
+                      <p className="mt-1">$16.00</p>
+                    </div>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Tshirt;
