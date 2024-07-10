06b0047b917fab9794c1c3583388a5ddf2c0c888
muHammadHammadali786Ha <sunm25377@gmail.com>
2024-07-09 18:30:53 -0700

    single product page desgin

--- src/app/tshirt/page.js
+++ src/app/tshirt/page.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import Link from 'next/link';
 import React from 'react';
 
@@ -11,37 +12,37 @@ const Tshirt = () => {
     },
     {
       id: 2,
-      img: "/tshirt1.jpg",
+      img: "/tshirt2.jpg",
       name: "The Catalyzer",
       category: "tShirt"
     },
     {
       id: 3,
-      img: "/tshirt1.jpg",
+      img: "/tshirt3.png",
       name: "The Catalyzer",
       category: "tShirt"
     },
     {
       id: 4,
-      img: "/tshirt1.jpg",
+      img: "/tshirt4.png",
       name: "The Catalyzer",
       category: "tShirt"
     },
     {
       id: 5,
-      img: "/tshirt1.jpg",
+      img: "/tshirt5.png",
       name: "The Catalyzer",
       category: "tShirt"
     },
     {
       id: 6,
-      img: "/tshirt1.jpg",
+      img: "/tshirt6.png",
       name: "The Catalyzer",
       category: "tShirt"
     },
     {
       id: 7,
-      img: "/tshirt1.jpg",
+      img: "/tshirt7.png",
       name: "The Catalyzer",
       category: "tShirt"
     },
@@ -57,15 +58,15 @@ const Tshirt = () => {
     <div>
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-wrap -m-4">
+          <div className="flex flex-wrap w-full justify-center items-center">
             {
               list.map((item, index) => {
                 return (
-                  <Link key={index} href={`/tshirt/${item.id}`}>
+                  <Link key={index} href={`/tshirt/${item.id}`} className=''>
                   <div className="lg:w-1/4 md:w-1/2 p-4 w-full" >
-                    <a className="block relative h-48 rounded overflow-hidden">
-                      <img alt="ecommerce" className="w-full h-full block" src={item.img} />
-                    </a>
+                    <Link href={''} className="block relative h-48 rounded overflow-hidden">
+                      <img alt="ecommerce" className="w-full h-full block" src={item.img} width={'600px'}/>
+                    </Link>
                     <div className="mt-4">
                       <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">{item.category}</h3>
                       <h2 className="text-gray-900 title-font text-lg font-medium">{item.name}</h2>
